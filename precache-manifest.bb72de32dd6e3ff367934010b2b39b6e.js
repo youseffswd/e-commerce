4ec133e14bfe536da0d8faa45c6f4e45ee44550d
youseffswd <yousef.sayed.2.0.0.3.11.7@gmail.com>
2020-09-28 02:51:10 +0200

    Updates

---
+++ precache-manifest.bb72de32dd6e3ff367934010b2b39b6e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2097fc21a6095b899effad06033f748",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "74803a4e185a41b8eb01",
+    "url": "/home/static/css/3.11829350.chunk.css"
+  },
+  {
+    "revision": "a5fbcfe47f58e9dfebf2",
+    "url": "/home/static/css/main.af1a5636.chunk.css"
+  },
+  {
+    "revision": "a75714fc4f7aca04cece",
+    "url": "/home/static/js/0.3ad929f0.chunk.js"
+  },
+  {
+    "revision": "d79548f46d8e3f386073",
+    "url": "/home/static/js/10.99c9983e.chunk.js"
+  },
+  {
+    "revision": "a21e49184ab03471b929",
+    "url": "/home/static/js/11.4ad08e60.chunk.js"
+  },
+  {
+    "revision": "b955acb10f2a4334561c",
+    "url": "/home/static/js/12.82a20c5e.chunk.js"
+  },
+  {
+    "revision": "ca52c3dddb0210c6ee29",
+    "url": "/home/static/js/13.26d0cc9b.chunk.js"
+  },
+  {
+    "revision": "74803a4e185a41b8eb01",
+    "url": "/home/static/js/3.43ee586b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/home/static/js/3.43ee586b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c69152bb9a97144a296",
+    "url": "/home/static/js/4.c7498268.chunk.js"
+  },
+  {
+    "revision": "31597a1e61d2ecc967ef",
+    "url": "/home/static/js/5.e5642215.chunk.js"
+  },
+  {
+    "revision": "aafb1a42d9f36a4f8b50",
+    "url": "/home/static/js/6.1d903d31.chunk.js"
+  },
+  {
+    "revision": "f6c1dd8aff4d465230b1",
+    "url": "/home/static/js/7.8a236e11.chunk.js"
+  },
+  {
+    "revision": "d29d4a61a3c94ea80f56",
+    "url": "/home/static/js/8.82a93b2d.chunk.js"
+  },
+  {
+    "revision": "d67bf24b53c978ba2f50",
+    "url": "/home/static/js/9.b42a92be.chunk.js"
+  },
+  {
+    "revision": "a5fbcfe47f58e9dfebf2",
+    "url": "/home/static/js/main.d41a69b4.chunk.js"
+  },
+  {
+    "revision": "cc4464e23868450fd7f3",
+    "url": "/home/static/js/runtime-main.806625b6.js"
+  }
+]);
---
+++ static/js/13.26d0cc9b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpe-commerce"]=this["webpackJsonpe-commerce"]||[]).push([[13],{76:function(e,n,t){"use strict";t.r(n);var c=t(1),o=t.n(c);n.default=function(){return o.a.createElement("div",null,"404 page not found")}}}]);
+//# sourceMappingURL=13.26d0cc9b.chunk.js.map
